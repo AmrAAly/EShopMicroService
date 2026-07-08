a2ae12be6e129413c8132040f6c7feb4d17beb1b
AmrAA <a.atef@isit-sa.com>
2026-07-08 12:36:45 +0300

    add product catalog api and implmentaions

---
+++ src/services/catalog/Catalog.Api/Products/CreateProductCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.Api.Products
+{
+    public record CreateProductCommand(
+        List<string> Category,
+        string Name,
+        string Description,
+        string ImageFile,
+        decimal Price
+    ): ICommand<CreateProductResult>;
+
+    public record CreateProductResult(
+        Guid Id
+    );
+
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Implement the logic to create a product here
+            var result = new CreateProductResult(Guid.NewGuid());
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ src/services/catalog/Catalog.Api/Products/CreateProductEndPoint.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Reflection;
+
+namespace Catalog.Api.Products
+{
+
+    public record CreateProductResponse(Guid Id);
+    public record CreateProductRequest(
+        List<string> Category,
+        string Name,
+        string Description,
+        string ImageFile,
+        decimal Price
+    );
+
+    public class CreateProductEndPoint : ICarterModule
+    {
+        public async void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", CreateProduct)
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .WithSummary("Creates product")
+            .WithDescription("Create Product");
+
+
+             async Task<IResult> CreateProduct(CreateProductRequest request, ISender sender)
+            {
+                var command = request.Adapt<CreateProductRequest, CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}", response);
+            }
+        }
+    }
+}
